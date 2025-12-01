7448a13c37e7b8daf704dcbbd02bd259520a5062
pallavi-kapila <pallavikapila01@gmail.com>
2025-12-01 22:44:01 +0530

    Add additional variable declarations and console logs

--- JavaScript Practise/first.js
+++ JavaScript Practise/first.js
@@ -1,2 +1,12 @@
 fullName = "Tony Stark"
+$age = 45;
+var price = 99.99;
+let x = 10;
+const y = 20;
+var isFollow = true;
 console.log(fullName);
+console.log(age);
+console.log(price);
+console.log(x);
+console.log(y);
+console.log(isFollow);
